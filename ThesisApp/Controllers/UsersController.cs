512a41bdba15bc29743c3be613159091d017a3ce
Beka-IT <1904.01028@manas.edu.kg>
2023-10-20 02:59:51 -0400

    Removed unnecessary files

---
+++ ThesisApp/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using ThesisApp.Helpers;
+
+namespace ThesisApp.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class UsersController : ControllerBase
+{
+    public UsersController() {}
+    
+}
